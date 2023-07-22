4c1103ba0046de31427867d7cd2c4b3ab2414b84
Krypf <73819781+Krypf@users.noreply.github.com>
2023-07-22 21:40:10 +0900

    Add files via upload

---
+++ recent.sh
@@ -0,0 +1,5 @@
+open https://arxiv.org/list/hep-th/recent
+open https://arxiv.org/list/gr-qc/recent
+open https://arxiv.org/list/math-ph/recent
+open https://arxiv.org/list/quant-ph/recent
+open https://arxiv.org/list/hep-ph/recent
---
+++ start.sh
@@ -0,0 +1,4 @@
+open "/Applications/Bartender 4.app"
+open "/Applications/Weather Widget.app"
+open "/Applications/Firefox.app"
+open "/Applications/Brave Browser.app"
